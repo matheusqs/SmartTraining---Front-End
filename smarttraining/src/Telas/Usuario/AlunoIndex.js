7dffc14e621a4da27c9320f456d3f79d74a055e6
Felipe Gonçalves <goncalves.pipo2123@gmail.com>
2018-11-26 22:34:27 -0200

    fazendo os gráficos

--- smarttraining/src/Telas/Usuario/AlunoIndex.js
+++ smarttraining/src/Telas/Usuario/AlunoIndex.js
@@ -1,31 +1,105 @@
 import React from 'react';
-import {Button} from 'primereact/button';
-import {DataTable} from 'primereact/datatable';
-import {Column} from 'primereact/column';
+import {Chart} from 'primereact/chart';
 import {Header} from '../../Components/Header';
 import {Footer} from '../../Components/Footer';
 
 export class AlunoIndex extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = ({
+      ficha: {},
+      avaliacoes: []
+    });
   }
 
   componentDidMount = () => {
-    let url = `http://localhost:8080/servletweb?acao=MostrarUltimaFicha&codCpf=${this.props.location.state.user.cpf}`
+    let url = `http://localhost:8080/servletweb?acao=MostrarUltimaFicha&codCpf=${this.props.location.state.user.cpf}`;
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(resultado => this.setState({ficha: resultado}));
+
+    url = `http://localhost:8080/servletweb?acao=ListarAvaliacoes&codCpf=${this.props.location.state.user.cpf}`;
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(resultado => this.setState({avaliacoes: resultado}));
   }
 
   render(){
+    let treinos;
+    if(this.state.ficha.treinos){
+      treinos = this.state.ficha.treinos.map((treino, i) =>
+        <li key={i}>
+          <h2>Treino {treino.numero}</h2>
+          <table>
+            <tr>
+              <th>Aparelho</th>
+              <th>Exercício</th>
+              <th>Séries</th>
+              <th>Repetições</th>
+              <th>Peso</th>
+            </tr>
+            {treino.atividades.map((atividade, j) =>
+              <tr>
+                <th>{atividade.nroAparelho}</th>
+                <th>{atividade.nomExercicio}</th>
+                <th>{atividade.nroSeries}</th>
+                <th>{atividade.nroRepeticoes}</th>
+                <th>{atividade.qtdPeso}</th>
+              </tr>
+            )}
+          </table>
+        </li>
+      );
+    }
+
+    let aval = [];
+    let peso = [];
+    let perc = [];
+    for(let avaliacao of this.state.avaliacoes){
+      aval.push(avaliacao.data);
+      peso.push(avaliacao.peso)
+      perc.push(avaliacao.percentualGordura);
+    }
+    console.log(aval + ' ' + peso + ' ' + perc);  
+    let data = {
+      labels: aval,
+      datasets: [
+        {
+          label: 'Peso',
+          data: peso,
+          fill: false,
+          backgroundColor: '#00fff2',
+          borderColor: '#00fff2'
+        },
+        {
+          label: 'Percentual de gordura',
+          data: perc,
+          fill: false,
+          backgroundColor: '#00ff00',
+          borderColor: '#00ff00'
+        }
+      ]
+    };
+
     return(
       <div>
         <Header tipo={this.props.location.state.user.tipo} user={this.props.location.state.user}/>
 
         <div>
-          <div>
-            <p>Completar os gráficos!!!!</p>
-          </div>
-          <div>
-            <p>Completar os gráficos!!!!</p>
-          </div>
+            <Chart type='line' data={data}/>
+        </div>
+
+        <div>
+          <ul>{treinos}</ul>
         </div>
 
         <Footer/>
